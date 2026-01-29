aa4d827a8c2f5342fdaa63c19290c1e25b0e5faa
Salmaneelb <salmane.elboubakkari.seb@gmail.com>
2026-01-29 10:32:49 +0100

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int main(void)
+{
+    int somme = 0;
+    int i;
+
+    for (i = 1; i <= 1000; i++)
+    {
+        /* si divisible par 11 : on ignore le nombre */
+        if (i % 11 == 0)
+            continue;
+
+        /* si divisible par 5 ou par 7, on ajoute à la somme */
+        if (i % 5 == 0 || i % 7 == 0)
+        {
+            somme += i;
+
+            /* si la somme dépasse 5000, on arrête la boucle */
+            if (somme > 5000)
+                break;
+        }
+    }
+
+    printf("Somme finale = %d\n", somme);
+
+    return 0;
+}
